2edc96b1d3c634c403bf01e6651ec2e020357c1c
Yunus <yunus.can42320@gmail.com>
2023-07-17 23:52:27 +0200

    17/07/2023

--- home/resume-mini/src/App.js
+++ home/resume-mini/src/App.js
@@ -1,71 +1,34 @@
 import React, { useState, useEffect } from "react";
-import { FaAngleDoubleRight } from "react-icons/fa";
-//import data from './data'
-
-const url = "https://course-api.com/react-tabs-project";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
-  const [value, setValue] = useState(0);
 
-  const fetchJobs = async () => {
-    const response = await fetch(url);
-    const newJobs = await response.json();
-    setJobs(newJobs);
-    setLoading(false);
-  };
+  const [people, setPeople] = useState(data);
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    fetchJobs();
-  }, []);
-
-  // before fetch
-  if (loading) {
-    return (
-      <section className="section loading">
-        <h2>Loading...</h2>
-      </section>
-    );
-  }
-
-  // after fetch
-  const { company, duties, dates, title } = jobs[value];
 
   return (
     <section className="section">
-      <div className="title">
-        <h2>Experience</h2>
-        <div className="underline"></div>
-      </div>
-      <div className="jobs-center">
-        <div className="btn-container">
-          {jobs.map((job, index) => {
-            return (
-              <button
-                key={index}
-                onClick={() => setValue(index)}
-                className={`job-btn ${index === value && "active-btn"}`}
-              >
-                {job.company}
-              </button>
+        <div className="title">
+          <h2><span>slider</span>person</h2>
+        </div>
+        <div className="section-center">
+          {people.map((person,index) => {
+            const {id,image,name,title,quote} = person;
+            
+            return(
+              <article key={id}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+              </article>
             );
           })}
         </div>
-        <article className="job-info">
-          <h3>{title}</h3>
-          <h4>{company}</h4>
-          <p className="job-date">{dates}</p>
-          {duties.map((duty, index) => {
-            return (
-              <div key={index} className="job-desc">
-                <FaAngleDoubleRight className="job-icon" />
-                <p>{duty}</p>
-              </div>
-            );
-          })}
-        </article>
-      </div>
     </section>
   );
 }
